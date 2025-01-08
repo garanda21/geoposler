4786c6b2edf386200e5f9a20f25f34f1f7d15750
Giovanny Aranda <garanda21@gmail.com>
2025-01-08 17:01:13 +0100

    Update Navbar component styling and replace icon with logo image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Mail } from 'lucide-react';
+import logo from '../icon.png';
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white shadow-sm">
+    <nav className="bg-[#16161E] shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
             <div className="flex items-center">
-              <Mail className="h-8 w-8 text-indigo-600" />
+              <img src={logo} alt="Geoposler Logo" className="h-8 w-10" />
               <div className="flex flex-col ml-2 items-start">
-              <h1 className="text-2xl font-bold text-gray-900">Geoposler</h1>
-              <p className="text-sm text-gray-500">Email Campaign Manager</p>
+                <h1 className="text-2xl font-bold text-white">Geoposler</h1>
+                <p className="text-sm text-gray-300">Email Campaign Manager</p>
               </div>
             </div>
         </div>
